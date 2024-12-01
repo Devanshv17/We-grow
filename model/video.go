9057b18837b3ab25901847278c7554af9f83aae2
Devansh <devanshv17@gmail.com>
2024-12-01 19:57:04 +0530

    title added

--- model/video.go
+++ model/video.go
@@ -4,5 +4,6 @@ type Video struct {
 	Link        string   `json:"link"`
 	Tags        []string `json:"tags"`
 	Creator     string   `json:"creator"`
+	Title       string   `json:"title"`
 	Description string   `json:"description"`
 }
