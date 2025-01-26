a9252fe10252534d11f2ad366e6108f4d0b3f73b
Devansh <devanshv17@gmail.com>
2025-01-26 15:39:47 +0530

    flag added

--- model/post.go
+++ model/post.go
@@ -11,13 +11,18 @@ type Post struct {
 	IsResolved bool               `json:"is_resolved"`
 	Tags       []string           `json:"tags"`     // New field for tags
 	Comments   map[string]Comment `json:"comments"` // Comments stored as a map of Comment structs
+	Flags      map[string]bool    `json:"flags"`    // Stores usernames who flagged the post
+	FlagCount  int                `json:"flag_count"`
 }
 
+// Comment represents a comment on a post
 type Comment struct {
-	ID        string `json:"id"`
-	Username  string `json:"username"` // Replaced userID with username
-	Content   string `json:"content"`
-	CreatedAt int64  `json:"created_at"`
-	IsAdmin   bool   `json:"is_admin"`
-	Role      string `json:"role"`
+	ID        string          `json:"id"`
+	Username  string          `json:"username"` // Replaced userID with username
+	Content   string          `json:"content"`
+	CreatedAt int64           `json:"created_at"`
+	IsAdmin   bool            `json:"is_admin"`
+	Role      string          `json:"role"`
+	Flags     map[string]bool `json:"flags"` // Stores usernames who flagged the comment
+	FlagCount int             `json:"flag_count"`
 }
