a91ab1de0809034265a78fedb764a4c5acaaef12
Devansh <devanshv17@gmail.com>
2025-02-04 01:00:24 +0530

    citation

--- model/video.go
+++ model/video.go
@@ -10,5 +10,5 @@ type Video struct {
 	IsTopVideo  bool     `json:"isTopVideo"`
 	Thumbnail   string   `json:"thumbnail"`
 	Rank        int      `json:"rank"`
-	Citation    string   `json:"Citation`
+	Citations   string   `json:"Citations`
 }
